81c3b0fb3027a79abc992fda5812a01a05306cf0
Baron Von Cat <110116982+BaronVonCat@users.noreply.github.com>
2025-02-11 10:23:51 +0300

    Delete Stolknoveniya/Assets directory

--- Stolknoveniya/Assets/Scripst/CubeFactory.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class CubeFactory
-{
-    public ExplosiveCube Create(ExplosiveCube explosionCube)
-    {
-        return Object.Instantiate(explosionCube, explosionCube.transform.position, Quaternion.identity);
-    }
-}
